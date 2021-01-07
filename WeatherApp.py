e404d1db4c3dd401ad6560dff11f143d527eca8e
Alex <alex.gyumushyan@gmail.com>
2021-01-07 18:10:51 -0500

    reworked WeatherReport function and added a simple label to show current temperature

--- WeatherApp.py
+++ WeatherApp.py
@@ -4,16 +4,41 @@ import requests
 
 
 def main():
-    return 
-    #used for GUI and such
+    window = Tk()
+    window.title("Weather App")
+    window.configure(background = "white")
+
+    data = WeatherReport()
+
+    Label(window, text = (f"the temperature is {data['temperature']}"), bg='white', fg = 'black').grid(row=1, column=1,sticky=W)
+    
+    window.mainloop()
+
+    
 
 
 def WeatherReport(cityName = "Toronto", key = "1a6e299186e76c80b2ff984ceebf2725"):
 
     response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid={key}")
     weatherReport = response.json()
-    return weatherReport #returns the weather report in a json format
+    #returns the weather report in a json format
+
+    outputDict = {}
+    
+
+    outputDict['main'] = weatherReport['weather'][0]['main']
+    outputDict['temperature'] = weatherReport['main']['temp']
+    outputDict['humidity'] = weatherReport['main']['humidity']
+    outputDict['windspeed'] = weatherReport['wind']['speed']
+    outputDict['wind direction'] = weatherReport['wind']['deg']
+
+    return outputDict
+
+   
 
 
 
+
+
+main()
 
