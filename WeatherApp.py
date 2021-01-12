679d95eb3fd12b8c70ca408697bdc8f019a02624
Alex <alex.gyumushyan@gmail.com>
2021-01-11 23:14:53 -0500

    added textvariables to fix the way the labels update the data

--- WeatherApp.py
+++ WeatherApp.py
@@ -13,11 +13,11 @@ def updateCity():
         cityName = "Toronto"
 
     data = WeatherReport(cityName)
-
-    updateTemp = Label(window, text = (f"The temperature in {cityName} is {data['temperature']}"), bg='white', fg = 'black').grid(row=0, column=1,sticky=W)
-    updateHumidity = Label(window, text = (f"The humidity in {cityName} is {data['humidity']} grams per kg of air"), bg='white', fg = 'black').grid(row=1, column=1,sticky=W)
-    updateWindspeed = Label(window, text = (f"The windspeed in {cityName} is {data['windspeed']} kilometers per hour"), bg='white', fg = 'black').grid(row=2, column=1,sticky=W)
     
+   
+    textTemperature.set(f"The temperature in {cityName} is {data['temperature']}")
+    textHumidity.set(f"The humidity in {cityName} is {data['humidity']}")
+    textWindspeed.set(f"The windpseed in {cityName} is {data['windspeed']}")
 
 
 
@@ -28,16 +28,27 @@ def main():
     window.configure(background = "white")
 
     #Label(window, text = (f"the temperature in Toronto is {round(data['temperature'],2)}"), bg='white', fg = 'black').grid(row=0, column=0,sticky=W)
-    Label(window, text = "please enter the name of the City you want to search", bg = "white", fg = "black").grid(row=1,column=0,sticky=W)
+    Label(window, text = "please enter the name of the City you want to search", bg = "white", fg = "black").grid(row=0,column=0,sticky=W)
 
     global cityInput
     cityInput = Entry(window, width = 20, bg = "white")
     cityInput.grid(row=2, column = 0, sticky=W)
 
-    Button(window, text = "Submit", width = 6, command = updateCity).grid(row = 3, column=0, sticky=W)
+    #updateCity()
+    global textTemperature
+    textTemperature = StringVar()
 
-    updateCity()
-    
+    global textHumidity
+    textHumidity = StringVar()
+
+    global textWindspeed
+    textWindspeed = StringVar()
+
+    Button(window, text = "Submit", width = 6, command = updateCity).grid(row = 3, column=0, sticky=W) 
+
+    Label(window, textvariable = textTemperature, bg='white', fg = 'black').grid(row=0, column=1,sticky=W)
+    Label(window, textvariable = textHumidity, bg='white', fg = 'black').grid(row=1, column=1,sticky=W)
+    Label(window, textvariable = textWindspeed, bg='white', fg = 'black').grid(row=2, column=1,sticky=W)
 
     window.mainloop()
 
@@ -61,7 +72,6 @@ def WeatherReport(cityName = "Toronto", key = ApiKey.key()):
 
     return outputDict
 
-   
 
 
 main()
