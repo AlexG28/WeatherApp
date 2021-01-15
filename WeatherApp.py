6f101c7bf0122bb9eeea1f0c81f89c6953828f04
Alex <alex.gyumushyan@gmail.com>
2021-01-14 21:30:38 -0500

    fixed the weather widget icons

--- WeatherApp.py
+++ WeatherApp.py
@@ -5,7 +5,6 @@ import ApiKey
 
 
 
-
 def updateCity():
     cityName = cityInput.get()
 
@@ -13,13 +12,27 @@ def updateCity():
         cityName = "Toronto"
 
     data = WeatherReport(cityName)
-    
    
     textTemperature.set(f"The temperature in {cityName} is {data['temperature']}")
     textHumidity.set(f"The humidity in {cityName} is {data['humidity']}")
     textWindspeed.set(f"The windpseed in {cityName} is {data['windspeed']}")
 
+    
+    #os.chdir('C:\Users\Aleksandr\Documents\Python_Programming\WeatherApp')
+    if data['main'] == 'Clouds':
+        photo1.configure(file='weatherSymbols\\cloudy.png')
+    elif data['main'] == 'Clear':
+        photo1.configure(file='weatherSymbols\\sunny.png')
+    elif data['main'] == 'Snow':
+        photo1.configure(file='weatherSymbols\\snowy.png')
+    else:
+        photo1.configure(file='weatherSymbols\\raining.png')
 
+    
+    
+    #photo1.configure(file='raining.png')
+    
+    
 
 def main():
     global window 
@@ -27,14 +40,10 @@ def main():
     window.title("Weather App")
     window.configure(background = "white")
 
-    #Label(window, text = (f"the temperature in Toronto is {round(data['temperature'],2)}"), bg='white', fg = 'black').grid(row=0, column=0,sticky=W)
-    Label(window, text = "please enter the name of the City you want to search", bg = "white", fg = "black").grid(row=0,column=0,sticky=W)
-
     global cityInput
     cityInput = Entry(window, width = 20, bg = "white")
-    cityInput.grid(row=2, column = 0, sticky=W)
-
-    #updateCity()
+    cityInput.grid(row=1, column = 0, sticky=W)
+    
     global textTemperature
     textTemperature = StringVar()
 
@@ -44,11 +53,23 @@ def main():
     global textWindspeed
     textWindspeed = StringVar()
 
-    Button(window, text = "Submit", width = 6, command = updateCity).grid(row = 3, column=0, sticky=W) 
+    global photo1
+    photo1 = PhotoImage()   
+   
+
+    #global photoLabel
+    
+
+    Label(window, text = "please enter the name of the City you want to search", bg = "white", fg = "black").grid(row=0,column=0,sticky=W)
+    Button(window, text = "Submit", width = 6, command = updateCity).grid(row = 2, column=0, sticky=W) 
 
     Label(window, textvariable = textTemperature, bg='white', fg = 'black').grid(row=0, column=1,sticky=W)
     Label(window, textvariable = textHumidity, bg='white', fg = 'black').grid(row=1, column=1,sticky=W)
     Label(window, textvariable = textWindspeed, bg='white', fg = 'black').grid(row=2, column=1,sticky=W)
+
+    Label(window, image = photo1).grid(row =0, column = 0, sticky=W)
+    updateCity()
+
 
     window.mainloop()
 
@@ -63,7 +84,6 @@ def WeatherReport(cityName = "Toronto", key = ApiKey.key()):
 
     outputDict = {}
     
-
     outputDict['main'] = weatherReport['weather'][0]['main']
     outputDict['temperature'] = weatherReport['main']['temp']
     outputDict['humidity'] = weatherReport['main']['humidity']
